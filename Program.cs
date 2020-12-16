94e4c05063ce1693dfb14ae5ae1390d5cacf0d82
DEV <dev@info.local>
2020-12-16 10:18:37 -0300

    EX 1 finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FixicacaoAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Em que ano você nasceu? ");
+            int ano = int.Parse(Console.ReadLine());
+                int idade = (2020 - ano);
+
+            if(idade >=18){
+                Console.WriteLine("Pode votar!");
+            }
+            else {
+                Console.WriteLine("De acordo com a sua idade, não esta apto para votar.");
+            }
+        }
+    }
+}
